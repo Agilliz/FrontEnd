d974e67f5129e076eccb707a68c9ee6ad1ae14a0
arthursilemanali <arthur.salomaoneto@sptech.school>
2024-06-21 16:49:19 -0300

    @arthursilemanali - removendo asneira

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL = process.env.REACT_APP_API_URL;
 
-console.log('HAHAHAHA SEU BLUEPILL, POUCA PIKA, ' + baseURL);
+console.log(baseURL);
 
 const api = axios.create({
      
